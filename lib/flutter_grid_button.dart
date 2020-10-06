865cde3b32e558155670b389ea8e59ef32121511
Naveen <metanav@users.noreply.github.com>
2020-10-06 20:08:16 +0900

    Update flutter_grid_button.dart

--- lib/flutter_grid_button.dart
+++ lib/flutter_grid_button.dart
@@ -37,7 +37,7 @@ class GridButton extends StatefulWidget {
   final List<List<GridButtonItem>> items;
 
   /// Called when the button is tapped or otherwise activated.
-  final ValueChanged<Widget, dynamic> onPressed;
+  final ValueChanged<dynamic> onPressed;
 
   /// The color to use when painting the line.
   final Color borderColor;
@@ -104,13 +104,13 @@ class _GridButtonState extends State<GridButton> {
           onPressed: (widget.enabled == true)
               ? () {
                   widget
-                      .onPressed(widget, item.value != null ? item.value : item.title);
+                      .onPressed({"widget": widget, "value": item.value != null ? item.value : item.title});
                 }
               : null,
           onLongPress: (widget.enabled == true)
               ? () {
                   var result = item.longPressValue ?? item.value;
-                  widget.onPressed(widget, result != null ? result : item.title);
+                  widget.onPressed({"widget": widget, "value": result != null ? result : item.title});
                 }
               : null,
           child: Text(
